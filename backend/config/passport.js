52b0a2d2a0c2ae06c3f49d839dce7488a588778c
AyushSingh2406 <ayushsingh2004.abs@gmail.com>
2025-08-22 01:12:33 +0530

    update

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,23 +5,19 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        // This is the fix: Use the full URL of your deployed backend
         callbackURL: 'https://reunite-vh55.onrender.com/api/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
         try {
-            // Check if user already exists in our database
             let user = await User.findOne({ email: profile.emails[0].value });
 
             if (user) {
-                // If user exists, but doesn't have a googleId, link the account
                 if (!user.googleId) {
                     user.googleId = profile.id;
                     await user.save();
                 }
                 done(null, user);
             } else {
-                // If user doesn't exist, create a new one
                 const newUser = new User({
                     googleId: profile.id,
                     username: profile.displayName,
@@ -40,7 +36,13 @@ module.exports = function(passport) {
         done(null, user.id);
     });
 
-    passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => done(err, user));
+    // This function has been updated to use the modern async/await syntax
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (err) {
+            done(err, null);
+        }
     });
 };
